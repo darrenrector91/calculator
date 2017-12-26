53439de89880eb5a615b52e2843df87ced21cdb0
darrenrector91 <darrenrector91@gmail.com>
2017-12-26 00:27:09 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var port = 5050;
+var bodyParser = require('body-parser');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start up server
+app.listen(port, function () {
+    console.log('listening on port: ', port);
+});
